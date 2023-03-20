1fa5d1e83bde239b78d89496080a9ec962c2b9cd
kbasha879 <121991051+kbasha879@users.noreply.github.com>
2023-03-20 17:36:57 +0530

    commited

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIAY7N63D3PJN4CY47R"
-secret_key = "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"
+access_key = "AKIAY7N63D3PDQUDVMAR"
+secret_key = "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"
 }
