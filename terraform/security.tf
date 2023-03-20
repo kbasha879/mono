77f89482cc32505ce86cc4167b336843e58ec5d6
kbasha879 <121991051+kbasha879@users.noreply.github.com>
2023-03-20 17:29:49 +0530

    commited

--- terraform/security.tf
+++ terraform/security.tf
@@ -19,7 +19,7 @@ cidr_blocks = ["0.0.0.0/0"]
 ingress {
 from_port = 0
 to_port = 0
-ptotocol = "-1"
+protocol = "-1"
 cidr_blocks = ["0.0.0.0/0"]
 }
 
@@ -27,7 +27,7 @@ cidr_blocks = ["0.0.0.0/0"]
 egress {
 from_port = 0
 to_port = 0
-ptotocol = "-1"
+protocol = "-1"
 cidr_blocks = ["0.0.0.0/0"]
 }
 }
