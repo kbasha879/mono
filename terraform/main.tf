efc506095aa354ee71cc54eced1a9e975a3f3abb
kbasha879 <121991051+kbasha879@users.noreply.github.com>
2023-03-20 17:32:44 +0530

    hghfghcj

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "key"{
 ami = "ami-0d81306eddc614a45"
 instance_type= "t2.micro"
-key_name = basha
+key_name = "basha"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
-tags {
-name = mono-deploy
+tags = {
+Name = "mono-deploy"
 Environment = "dev"
 }
 }
